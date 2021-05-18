44cf4448e44408bcc17ed9a264c60b9ab22002ec
martinezlucas98 <lucasoctaviom@gmail.com>
2021-05-18 15:56:27 -0400

    Add for-in/foreach statement example on examples/modules/for_loop_test.java

--- examples/modules/for_loop_test.java
+++ examples/modules/for_loop_test.java
@@ -2,6 +2,9 @@ public class Main {
   public static void main(String[] args) {
     for (int i = 0; i < 5; i++) {
       // code block to be executed
-    }  
+    }
+    for (int i : array) {
+      // something
+    }
   }
 }
